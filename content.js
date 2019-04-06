3e80606dd3c1ff686522ada82b3bee6e9d6abdc6
yesass <assem.yeskabyl@nu.edu.kz>
2019-04-06 21:11:23 +0600

    changed the pic

--- content.js
+++ content.js
@@ -1,6 +1,15 @@
 window.addEventListener('load',
     function(details) {
         //document.getElementsByClassName("p");
-		document.body.innerHTML += "<p>test succeeded</p>";
+  		document.body.innerHTML += "<p>test </p>";
+    }
+);
+
+window.addEventListener('click',
+    function(details) {
+        //document.getElementsByClassName("p");
+      var someimage = document.getElementById('first_id');
+      var edit_save = someimage.getElementsByTagName('img')[0];
+      edit_save.src = "darknet/data/food.jpg";
     }
 );
