34c3f8527b539a374f568c4b8e250b6bed268980
kamdibus <kam.dibus@gmail.com>
2019-04-06 23:22:28 +0200

    tags

--- popup.js
+++ popup.js
@@ -1,32 +1,33 @@
 function updateLabel() {
-    var enabledPlugin;
     chrome.storage.sync.get("enabledPlugin", function(result){
-        enabledPlugin = result.enabledPlugin;
+        var enabledPlugin = result.enabledPlugin;
         document.getElementById('toggle_button').value = enabledPlugin ? "Disable" : "Enable";
     });
 }
 
-// function click() {
-//  var enabledPlugin;
-//  chrome.storage.sync.get("enabledPlugin", function(result){
-//    enabledPlugin = result.enabledPlugin;
-//    chrome.storage.sync.set({"enabledPlugin": !enabledPlugin});
-//    updateLabel();
-//  });
-// }
+function updateTags() {
+    chrome.storage.sync.get("tags", function(result){
+        var tags = result.tags;
+        document.getElementById('tags').value = tags;
+    });
+}
 
 window.onload = function () {
+    updateTags();
     updateLabel();
 }
 
 window.setTimeout(function() {
     document.getElementById("toggle_button").onclick = function() {
-        console.log('klik');
         chrome.storage.sync.get("enabledPlugin", function(result){
             var enabledPlugin = result.enabledPlugin;
-            console.log(enabledPlugin);
             chrome.storage.sync.set({"enabledPlugin": !enabledPlugin});
             updateLabel();
         });
     }
+
+    document.getElementById("tags").onchange = function() {
+        var tags = document.getElementById("tags").value;
+        chrome.storage.sync.set({"tags": tags});
+      }
 }, 500);
