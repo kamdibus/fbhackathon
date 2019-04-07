5292c0ec3293165a536a4a107548313da1fa46d5
kamdibus <kam.dibus@gmail.com>
2019-04-07 05:02:29 +0200

    async

--- content.js
+++ content.js
@@ -1,15 +1,15 @@
 //global arrays with references
 
 var images = [],
-	bg_images = [],
-	image_parents = [];
+  bg_images = [],
+  image_parents = [];
 
 var jake = "https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/3/3b/Jakesalad.png/revision/latest?cb=20160503014517";
 var bmo = "https://images.discordapp.net/avatars/418412306981191680/2f2a22feae70021176e32fc8cdf91a98.png?size=512";
 
 var our_images = [
-	jake,
-	bmo
+  jake,
+  bmo
 ];
 
 var cached_imgs = new Object();
@@ -17,51 +17,51 @@ var cached_imgs = new Object();
 var SIZE_LIMIT = 200;
 
 function isBigEnough(image) {
-	return (image.height > SIZE_LIMIT || image.width > SIZE_LIMIT);
+  return (image.height > SIZE_LIMIT || image.width > SIZE_LIMIT);
 }
 
 function willBeChecked(image) {
-	if (!isBigEnough(image))
-		return false;
+  if (!isBigEnough(image))
+    return false;
 
-	if (image.src) {
-		 return (!Object.keys(cached_imgs).includes(image.src));
-	}
-	return false;
+  if (image.src) {
+    return (!Object.keys(cached_imgs).includes(image.src));
+  }
+  return false;
 }
 
 function shouldBeChanged(image, tag) {
-	var result = false;
-	if (!isBigEnough(image))
-		return false;
-	console.log('badam obrazek');
-	console.log(image.src);
+  var result = false;
+  if (!isBigEnough(image))
+    return false;
+  console.log('badam obrazek');
+  console.log(image.src);
 
-	if (image.src) {
-		if (!localStorage.getItem(image.src)) {
-			console.log('nowy!' + image.src);
-			if (isDisallowed([tag], image.src)) {
-				localStorage.setItem(image.src, tag);
-				return true;
-			}
-			else {
-				localStorage.setItem(image.src, '%');
-				return false;
-			}
-		}
-		else {
-			return (localStorage.getItem(image.src) == tag);
-		}
-	}
-	console.log('oto result:');
-	console.log(result);
-	return result;
+  if (image.src) {
+    if (!localStorage.getItem(image.src)) {
+      console.log('nowy!' + image.src);
+      isDisallowed([tag], image.src, function(isDis) {
+        if (isDis) {
+          localStorage.setItem(image.src, tag);
+          return true;
+        } else {
+          localStorage.setItem(image.src, '%');
+          return false;
+        }
+      });
+    } else {
+      return (localStorage.getItem(image.src) == tag);
+    }
+  }
+  console.log('oto result:');
+  console.log(result);
+  return result;
 }
 
 var isAllDead = false;
 
 function isAllDisabled() {
-	return isAllDead;
+  return isAllDead;
 }
 
 
@@ -78,100 +78,101 @@ function isAllDisabled() {
 
 var imgs_to_check = [];
 
-window.addEventListener('load', function () {
-	var body = document.body;
-	var elements = document.body.getElementsByTagName("*");
+window.addEventListener('load', function() {
+  var body = document.body;
+  var elements = document.body.getElementsByTagName("*");
 
-	var howMany = 0;
-	for (i in document.images) {
-			var image = document.images[i];
-			if (willBeChecked(image))
-				howMany++;
-			if (shouldBeChanged(image)) {
-				console.log('zrodlo 1');
-				
-				image.srcset = newSrcList;
-				image.src = bmo;
-			}
-	} //szybciej 1
-	console.log('partia 1: ' + howMany.toString());
-	
+  var howMany = 0;
+  for (i in document.images) {
+    var image = document.images[i];
+    if (willBeChecked(image))
+      howMany++;
+    if (shouldBeChanged(image, "dog")) {
+      console.log('zrodlo 1');
+
+      image.srcset = newSrcList;
+      image.src = bmo;
+    }
+  } //szybciej 1
+  console.log('partia 1: ' + howMany.toString());
 
 
-	/* MutationObserver callback to add images when the body changes */
-	var callback = function( mutationsList, observer ){
-		chrome.storage.sync.get("enabledPlugin", function(result) {
-			if (result === undefined)
-				return;
 
-			if (result.enabledPlugin) {
+  /* MutationObserver callback to add images when the body changes */
+  var callback = function(mutationsList, observer) {
+    chrome.storage.sync.get("enabledPlugin", function(result) {
+      if (result === undefined)
+        return;
 
-				var howMany2 = 0;
-				for (i = 0; i < mutationsList.length; i++ ) {
-					var mutation = mutationsList[i];
-					if (mutation.target.tagName == "IMG") {
-							if (!our_images.includes(mutation.target.src)) {
-								if (willBeChecked(mutation.target))
-									howMany2++;
-								if (shouldBeChanged(mutation.target)) {
-									console.log('zrodlo 2');
-									
-									mutation.target.src = bmo;
-									mutation.target.srcset = newSrcList;
-								}
-							}
-						}
-					
+      if (result.enabledPlugin) {
 
-					if ( mutation.type == 'childList' ) {
-						var howMany = 0;
-						var imagesChildren = mutation.target.getElementsByTagName("IMG");
-						for (imgChild in imagesChildren) {
-							if (willBeChecked(imagesChildren[imgChild]))
-								howMany++;
-							if (shouldBeChanged(imagesChildren[imgChild])) {
-								console.log('zrodlo 3');
-								
-								imagesChildren[imgChild].src = bmo;
-								imagesChildren[imgChild].srcset = newSrcList;
-							}
-						} 
-						console.log('partia zawierała ' + howMany.toString());
-						//szybciej 3
-						var howMany3 = 0;
+        var howMany2 = 0;
+        for (i = 0; i < mutationsList.length; i++) {
+          var mutation = mutationsList[i];
+          if (mutation.target.tagName == "IMG") {
+            if (!our_images.includes(mutation.target.src)) {
+              if (willBeChecked(mutation.target))
+                howMany2++;
+              if (shouldBeChanged(mutation.target, "dog")) {
+                console.log('zrodlo 2');
 
-						// Array.prototype.forEach.call( mutation.target.children, function ( child ) {
-						// 	var style = child.currentStyle || window.getComputedStyle(child, false);
-						// 	if ( child.tagName === "IMG" || child.tagName === "img") {
-						// 		images.push( child.src ); // save image src
-						// 		image_parents.push( child.parentNode ); // save image parent
-						// 		if (shouldBeChanged(child)) {
-						// 			howMany3++;
-						// 			child.src = jake;
-						// 		}
-						// 	}
-						// 	if ( style.backgroundImage != "none" ) {
-						// 		bg_images.push( style.backgroundImage.slice( 4, -1 ).replace(/['"]/g, ""));
-						// 	}
+                mutation.target.src = bmo;
+                mutation.target.srcset = newSrcList;
+              }
+            }
+          }
 
-						// } );
-						console.log('partia4 zawiera ' + howMany3.toString());
-					}
-				}
-				console.log('partia3 zawiera ' + howMany2.toString());
-			}
-			console.log('ile w cache? ' + Object.keys(cached_imgs).length.toString());
-		});
-	}
 
-	var observer = new MutationObserver( callback );
-	var config = { 
-					characterData: true,
-					attributes: true,
-					childList: true,
-					subtree: true };
+          if (mutation.type == 'childList') {
+            var howMany = 0;
+            var imagesChildren = mutation.target.getElementsByTagName("IMG");
+            for (imgChild in imagesChildren) {
+              if (willBeChecked(imagesChildren[imgChild]))
+                howMany++;
+              if (shouldBeChanged(imagesChildren[imgChild], "dog")) {
+                console.log('zrodlo 3');
 
-	observer.observe( body, config );
+                imagesChildren[imgChild].src = bmo;
+                imagesChildren[imgChild].srcset = newSrcList;
+              }
+            }
+            console.log('partia zawierała ' + howMany.toString());
+            //szybciej 3
+            var howMany3 = 0;
+
+            // Array.prototype.forEach.call( mutation.target.children, function ( child ) {
+            // 	var style = child.currentStyle || window.getComputedStyle(child, false);
+            // 	if ( child.tagName === "IMG" || child.tagName === "img") {
+            // 		images.push( child.src ); // save image src
+            // 		image_parents.push( child.parentNode ); // save image parent
+            // 		if (shouldBeChanged(child)) {
+            // 			howMany3++;
+            // 			child.src = jake;
+            // 		}
+            // 	}
+            // 	if ( style.backgroundImage != "none" ) {
+            // 		bg_images.push( style.backgroundImage.slice( 4, -1 ).replace(/['"]/g, ""));
+            // 	}
+
+            // } );
+            console.log('partia4 zawiera ' + howMany3.toString());
+          }
+        }
+        console.log('partia3 zawiera ' + howMany2.toString());
+      }
+      console.log('ile w cache? ' + Object.keys(cached_imgs).length.toString());
+    });
+  }
+
+  var observer = new MutationObserver(callback);
+  var config = {
+    characterData: true,
+    attributes: true,
+    childList: true,
+    subtree: true
+  };
+
+  observer.observe(body, config);
 });
 
 var newSrcList = bmo + ' 640w, ' + bmo + ' 750w, ' + bmo + ' 1080w';
--- kasper_magic.js
+++ kasper_magic.js
@@ -1,27 +1,26 @@
 var script = document.createElement('script');
- 
-script.src = '//code.jquery.com/jquery-1.11.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script); 
 
-function isDisallowed(tags, sourceImageUrl) {
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+function isDisallowed(tags, sourceImageUrl, nextAction) {
 	var subscriptionKey = "3ed0821c8842487585465180795285b8";
-	
+
 	var uriBase =
         "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze";
-	
+
 	var params = {
-        "visualFeatures": "Categories,Description",
+        "visualFeatures": "Description",
         "details": "",
         "language": "en",
     };
-	
+
 	var result = false;
-	
+
 	// Make the REST API call.
     $.ajax({
         url: uriBase + "?" + $.param(params),
 
-        // Request headers.
         beforeSend: function(xhrObj){
             xhrObj.setRequestHeader("Content-Type","application/json");
             xhrObj.setRequestHeader(
@@ -30,9 +29,8 @@ function isDisallowed(tags, sourceImageUrl) {
 
         type: "POST",
 
-        // Request body.
         data: '{"url": ' + '"' + sourceImageUrl + '"}',
-		async: false
+	      async: true
     })
 
     .done(function(data) {
@@ -40,31 +38,23 @@ function isDisallowed(tags, sourceImageUrl) {
 		for (i in tags) {
 			for (j in photoTags) {
 				if (photoTags[j] === tags[i]) {
-					result = true;
-					return;
+					nextAction(true);
+          return;
 				}
 			}
 		}
+    nextAction(false);
     })
 
     .fail(function(jqXHR, textStatus, errorThrown) {
+        nextAction(false);
         // Display error message.
-        var errorString = (errorThrown === "") ? "Error. " :
-            errorThrown + " (" + jqXHR.status + "): ";
-        errorString += (jqXHR.responseText === "") ? "" :
-            jQuery.parseJSON(jqXHR.responseText).message;
-        alert(errorString);
+        // var errorString = (errorThrown === "") ? "Error. " :
+        //     errorThrown + " (" + jqXHR.status + "): ";
+        // errorString += (jqXHR.responseText === "") ? "" :
+        //     jQuery.parseJSON(jqXHR.responseText).message;
+        // alert(errorString);
     });
-	
+
     console.log('czy rozpoznano? ' + result.toString() );
-
-	return result;
 };
-
-
-
-
-
-
-
-
